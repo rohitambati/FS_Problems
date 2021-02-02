7fcd7808adc2c6a250f7ccf94dc2080eadd97538
rohitambati <69076118+rohitambati@users.noreply.github.com>
2021-02-02 20:57:49 +0530

    Haven't solved yet

---
+++ Height.java
@@ -0,0 +1,25 @@
+/*
+
+Ramu is given a heights of students of 'n' Class Students
+
+Now Ramu is intrested to gather all the students at one place in increasing order of the heights of these classes 
+
+Help Ramu to solve  this problem
+
+The first line of input consists of the number of classrooms followed by each class students heights 
+
+input = 3
+1 4 5
+1 3 4
+2 6
+output = 1 1 2 3 4 4 5 6
+
+
+input = 3
+1 3 8 10
+2 5 8 9
+3 5 6 8 10
+output =1 2 3 3 5 5 6 8 8 8 9 10 10
+
+
+*/
